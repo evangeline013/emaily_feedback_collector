92f508828b73f226b7555ba6d65c0a9a7248bb4e
Eva <evangeline013@gmail.com>
2020-04-21 08:10:11 -0400

    redirect on logout and  add link tag for header title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -9,14 +10,19 @@ class Header extends Component {
             case false:
                 return <li><a href="/auth/google">Login With Google</a></li>;
             default:
-                return <li><a>Logout</a></li>;
+                return <li><a href="/api/logout">Logout</a></li>;
         }
     }
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="#" className="left brand-logo">Emaily</a>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
+                        Emaily
+                    </Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         {this.renderContent()}
                     </ul>
