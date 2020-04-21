dd9949d1c8dadda52306e09c2ee4a2c0de0f2257
Eva <evangeline013@gmail.com>
2020-04-21 07:55:11 -0400

    fetch user upon app rendering, access state in header to decide login/logout button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a>Logout</a></li>;
+        }
+    }
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
                     <a href="#" className="left brand-logo">Emaily</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -17,4 +26,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps(state) {
+    return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(Header);
