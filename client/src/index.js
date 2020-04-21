49a11f311302084de25809393ed7956eccff5e08
Eva <evangeline013@gmail.com>
2020-04-21 08:40:11 -0400

    set up stripe to handle payments: submit form from FE and finalize a charge from backend

--- client/src/index.js
+++ client/src/index.js
@@ -13,3 +13,6 @@ ReactDOM.render(
     <Provider store={store}><App /></Provider>,
     document.querySelector('#root')
 );
+
+console.log(process.env.REACT_APP_STRIPE_KEY);
+console.log('ENV is', process.env.NODE_ENV);
