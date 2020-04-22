8555d1e3f78a719f3a84d1dc319a5e4e54f3f6a3
Eva <evangeline013@gmail.com>
2020-04-22 06:34:19 -0400

    update user credits and add route specific middleware

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 // describe all different properties a user can have
 const userSchema = new Schema({
     googleId: String,
+    credits: { type: Number, default: 0 },
 });
 
 // create a new collection for users, if exsits, will not overwrite 
