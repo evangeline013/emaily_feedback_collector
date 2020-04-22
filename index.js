6fc5aedb0b6ab715c90aa4973510e7a6a114975f
Eva <evangeline013@gmail.com>
2020-04-22 07:47:48 -0400

    add client side routing in express for prod, configure heroku to build before deploy

--- index.js
+++ index.js
@@ -28,5 +28,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve main.js, main.css when req is looking for specific file
+    app.use(express.static('client/build'));
+
+    // Serve index.html when route is not recognized
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
