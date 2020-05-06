897acf3415f4c3a3c4c4af1a1fdfa56b3c2906e9
Eva <evangeline013@gmail.com>
2020-05-06 07:45:42 -0400

    add custom field component, dry up fields, buttons styling

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyFiled from './SurveyField';
+import { Link } from 'react-router-dom';
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' },
+];
 
 class SurveyForm extends Component {
+    renderFields() {
+        return (
+            FIELDS.map(({ label, name }) => {
+                return <Field key={name} label={label} type="text" name={name} component={SurveyFiled} />;
+            })
+        );
+    };
+
     render() {
         return (
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field type="text" name="surveyTitle" component="input" />
-                    <button type="submit">Submit</button>
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
